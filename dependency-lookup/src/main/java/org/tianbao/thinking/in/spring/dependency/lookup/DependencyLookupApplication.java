1e1b24c28b3c772d2651c2772ae48db64ad3b585
cuitao <cuitao@aixuexi.com>
2021-06-08 10:13:59 +0800

    add study notebook

---
+++ dependency-lookup/src/main/java/org/tianbao/thinking/in/spring/dependency/lookup/DependencyLookupApplication.java
@@ -0,0 +1,13 @@
+package org.tianbao.thinking.in.spring.dependency.lookup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyLookupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyLookupApplication.class, args);
+    }
+
+}
---
+++ dependency-lookup/src/test/java/org/tianbao/thinking/in/spring/dependency/lookup/DependencyLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tianbao.thinking.in.spring.dependency.lookup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyLookupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
